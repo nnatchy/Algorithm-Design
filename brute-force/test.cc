a0854bf369cb4fed2bf6b22c05ab50bfd5f1a319
Jirawat14 <natchjirawat@gmail.com>
2023-01-25 13:01:26 +0700

    updated

--- brute-force/test.cc
+++ brute-force/test.cc
@@ -1,9 +1,22 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 using namespace std;
 
+void combi(int n, vector<int>& sol, int len) {
+    if (len < n) {
+        sol[len] = 1;
+        combi(n, sol, len + 1);
+        sol[len] = 0;
+        combi(n, sol, len + 1);
+    } else {
+        for (int i = 0; i < n; i++) if (sol[i] == 1) cout << i + 1 <<  " ";
+        cout << "." << "\n";
+    }
+}
+
 int main() {
-    string tmp = " " + 1;
-    cout << tmp << "\n";
+    vector<int> sol(3);
+    combi(3, sol, 0);
 }
