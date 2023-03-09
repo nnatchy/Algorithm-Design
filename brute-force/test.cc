98f011a453b63f83ab70d3bf6a8549db2110cb31
Jirawat14 <natchjirawat@gmail.com>
2023-03-09 20:56:21 +0700

    closest pair

--- brute-force/test.cc
+++ brute-force/test.cc
@@ -21,17 +21,80 @@ int lb(vector<int> &dp, int target, int n) {
     return start;
 }
 
+int bs(vector<int> &dp, int target) {
+    int start = 0;
+    int stop = dp.size();
+    while (start < stop) {
+        int mid = start + (stop - start) / 2;
+        if (target <= dp[mid]) {
+            stop = mid;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return dp[start] == target ? start : -1;
+}
+
+int missing(vector<int> &dp) {
+    int start = 0;
+    int stop = dp.size();
+    while (start < stop) {
+        int mid = start + (stop - start) / 2;
+        if (mid < dp[mid]) {
+            stop = mid;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return start;
+}
+
+int peakDetect(vector<int> &dp, int left, int right) {
+    int n = dp.size();
+    if (right - left == 2) return dp[left + 1];
+    int mid = left + (right - left) / 2;
+    if (mid - 1 >= 0 && mid + 1 < n && dp[mid] > dp[mid - 1] && dp[mid] > dp[mid + 1]) return dp[mid];
+    if (dp[mid] > dp[mid - 1]) return peakDetect(dp, mid, right);
+    else return peakDetect(dp, left, mid);
+}
+
+int peak(vector<int> &dp) {
+    int start = 0;
+    int n = dp.size();
+    int stop = dp.size();
+    int ans = 0;
+    while (start < stop) {
+        int mid = start + (stop - start) / 2;
+        if (mid - 1 >= 0 && mid + 1 <= n - 1) {
+            if (dp[mid] >= dp[mid + 1] && dp[mid] >= dp[mid - 1]) {
+                start = mid;
+                break;
+            }
+        }
+        if (mid + 1 <= n - 1 && dp[mid] <= dp[mid + 1]) {
+            start = mid;
+        } else {
+            stop = mid - 1;
+        }
+    }
+    return dp[start];
+}
+
+int max_3(vector<int> &dp) {
+
+}
+
 int main() {
     vector<int> dp;
-    int n = 6;
+    int n = 10;
     int start = 0;
     for (int i = 0; i < n; i++) {
         ll inp;
         cin >> inp;
         dp.push_back(inp);
     }
-    int firstIdx = lb(dp, 1, n);
-    int secondIdx = lb(dp, 2, n);
-    cout << secondIdx - firstIdx << '\n';
+    // int target;
+    // cin >> target;
+    cout << peakDetect(dp, 0, n) << '\n';
 }
 
--- grader/dp&dc&sorting/ex02h1_closest.cpp
+++ grader/dp&dc&sorting/ex02h1_closest.cpp
@@ -1,19 +1,30 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 #define ll long long
 #define pll pair<ll,ll>
+#define inf 100000007;
 
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0);
     int N;
     cin >> N;
-    vector<pll> store;
-    while (N--) {
-        ll x, y;
-        cin >> x >> y;
-        store.push_back(make_pair(x, y));
+    vector<pll> store(N);
+    for (auto &p : store) {
+        cin >> p.first >> p.second;
     }
-    
+    sort(store.begin(), store.end());
+    int mn = inf;
+    int sz = 5;
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < min(N, i + sz); j++) {
+            int x = store[i].first - store[j].first;
+            int y = store[i].second - store[j].second;
+            mn = min(mn, x * x + y * y);
+        }
+    }
+    cout << mn << "\n";
 }
