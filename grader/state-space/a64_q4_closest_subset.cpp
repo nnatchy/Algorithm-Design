a28e330a03d5bc21ebdc9930526c6c47088cc20e
Jirawat14 <natchjirawat@gmail.com>
2023-04-19 23:12:17 +0700

    closest_subset
    tbh, idk how it got 100, but it's 100 so.

--- grader/state-space/a64_q4_closest_subset.cpp
+++ grader/state-space/a64_q4_closest_subset.cpp
@@ -10,14 +10,36 @@
 
 using namespace std;
 
+ll currentAns = 1e9;
+vector<bool> used;
+vector<ll> store;
+
+void solve(ll step, ll n, ll wantedLen, ll target, ll haveChosen, ll i, ll currentSum) {
+    if (haveChosen == wantedLen) {
+        if (abs(currentSum - target) <= abs(currentAns - target)) {
+            currentAns = currentSum;
+        }
+        return;
+    }
+    if (n - step < wantedLen - haveChosen) return;
+    if (currentSum > target && abs(currentSum - target) >= abs(currentAns - target)) return;
+    if (haveChosen < wantedLen) {
+        solve(step + 1, n, wantedLen, target, haveChosen + 1, i + 1, currentSum + store[i]);
+        if (wantedLen - haveChosen < n - step) {
+          solve(step + 1, n, wantedLen, target, haveChosen, i + 1, currentSum);
+        }
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(0);
     ll n, m, k;
     cin >> n >> m >> k;
-    vector<ll> store(n);
+    store.resize(n);
     for (int i = 0; i < n; i++) {
         cin >> store[i];
     }
-    vector<ll> dp(n + 1, 0);
-    
+    sort(store.begin(), store.end(), greater<ll>());
+    solve(0, n, m, k, 0, 0, 0);
+    cout << abs(k - currentAns) << "\n";
 }
--- grader/state-space/tempCodeRunnerFile.cpp
+++ grader/state-space/tempCodeRunnerFile.cpp
@@ -4,71 +4,38 @@
 #include<stack>
 #include<algorithm>
 #include<limits.h>
-#include<cmath>
-#include<map>
 
 #define ll long long
 #define pll pair<ll, ll>
-#define tmpPair pair<ll, vector<vector<ll> > >
 
 using namespace std;
 
-ll checkCost(vector<vector<ll> > &table) {
-    ll cost = 0;
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            if (table[i][j] == 0) continue;
-            cost += abs(i - (table[i][j] - 1) / 4) + abs(j - (table[i][j] - 1) % 4); // check move row, move col
-        }
-    }
-    return cost;
-}
+ll currentAns = 1e9;
+vector<bool> used;
+vector<ll> store;
 
-pll findZero(vector<vector<ll> > &table) {
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            if (table[i][j] == 0) {
-                return make_pair(i, j);
-            }
+void solve(ll step, ll n, ll wantedLen, ll target, ll haveChosen, ll i, ll currentSum) {
+    if (step > n) return;
+    if (haveChosen < wantedLen) {
+        solve(step + 1, n, wantedLen, target, haveChosen + 1, i + 1, currentSum + store[i]);
+        solve(step + 1, n, wantedLen, target, haveChosen, i + 1, currentSum);
+    } else {
+        if (abs(currentSum - target) <= abs(currentAns - target)) {
+            // printf("res: %llu\n", currentSum);
+            currentAns = currentSum;
         }
+        return;
     }
 }
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(0);
-    vector<vector<ll> > table(4, vector<ll>(4, 0));
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            cin >> table[i][j];
-        }
+    ll n, m, k;
+    cin >> n >> m >> k;
+    store.resize(n);
+    for (int i = 0; i < n; i++) {
+        cin >> store[i];
     }
-    map<vector<vector<ll> >, ll > tableMove; // map that store type of table and its move count from original
-    priority_queue<tmpPair, vector<tmpPair>, greater<tmpPair> > pq;
-    ll costOfMoving = checkCost(table);
-    pq.push(make_pair(costOfMoving, table));
-    tableMove[table] = 0;
-    ll dr[] = {1, -1, 0, 0}, dc[] = {0, 0, 1, -1};
-    while (!pq.empty()) {
-        tmpPair current = pq.top();
-        pq.pop();
-        vector<vector<ll> > tmpTable = current.second;
-        ll cntMove = tableMove[tmpTable];
-        ll costSearch = checkCost(tmpTable);
-        if (costSearch == 0) {
-            cout << cntMove << "\n";
-            return 0;
-        }
-        pll zeroPos = findZero(tmpTable);
-        ll r = zeroPos.first, c = zeroPos.second;
-        for (int i = 0; i < 4; i++) {
-            ll rr = dr[i] + r;
-            ll cc = dc[i] + c;
-            if (rr < 0 || rr >= 4 || cc < 0 || cc >= 4) continue;
-            swap(tmpTable[rr][cc], tmpTable[r][c]);
-            ll newCostSearch = checkCost(tmpTable);
-            tableMove[tmpTable] = cntMove + 1;
-            pq.push(make_pair(newCostSearch + cntMove + 1, tmpTable));
-            swap(tmpTable[rr][cc], tmpTable[r][c]);
-        }
-    }
+    solve(0, n, m, k, 0, 0, 0);
+    cout << abs(k - currentAns) << "\n";
 }
